181ef4cb9026ed22ecb84101db8fe70dbb668b27
lenschik.kor <lenschik.kor@yandex.ru>
2023-02-10 22:17:20 +0300

    Пример_2_через модуль

---
+++ primer_2_1/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Задача 26: Напишите программу, которая принимает на вход число и 
+выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+                else
+        {
+            Console.WriteLine("Число ввели некорректно.");
+        }
+    }
+
+    return Math.Abs(result);
+}
+ 
+ int LengthNumber(int numberes)
+ {string number1 = numberes.ToString();
+    int caunt = number1.Length;
+    return caunt;
+ }
+
+ int numberes = GetNumber("Введите число");
+ int caunt = LengthNumber(numberes);
+ Console.WriteLine ($"Число {numberes} содержит {caunt} цифр");
+
+
+
