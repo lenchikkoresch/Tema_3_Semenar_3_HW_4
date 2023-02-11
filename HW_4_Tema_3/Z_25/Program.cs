b7337400046d4f5107ccae07c0070518572367a3
lenschik.kor <lenschik.kor@yandex.ru>
2023-02-11 14:02:06 +0300

    Тема_3_семенар_4_задача_24

---
+++ HW_4_Tema_3/Z_25/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+int GetNumber(string messge)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(messge);
+        if(int. TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Вели не корректное число");
+        }
+    }
+    return result;
+}
+
+
+double GetDegree (int A, int B)
+{
+    double C = Math.Pow(A,Math.Abs(B));
+return C;
+}
+int A = GetNumber ("Введите число А");
+int B = GetNumber ("Введите число B");
+double C = GetDegree (A, B);
+Console.WriteLine($"Число {A} в натуральной степени {B} равно {C}");
+
