c23b2998d030f0488fbffd61ee5f69fd73995705
lenschik.kor <lenschik.kor@yandex.ru>
2023-02-11 09:31:51 +0300

    Пример_3

---
+++ Primer_3_28/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+Задача 28: Напишите программу, которая принимает на вход число N и 
+выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+/*
+int GetNumber(string message)
+{
+int result = 0;
+
+while (true)
+{
+Console.WriteLine(message);
+
+if (int.TryParse(Console.ReadLine(), out result) && result >= 1)
+{
+break;
+}
+else
+{
+Console.WriteLine("Ввели не число. Повторите ввод");
+}
+}
+return result;
+}
+int MultipleNumers (int number)
+{
+int multi = 1;
+for (int i = 1; i <= number; i++)
+{
+multi = multi * i;
+}
+return multi;
+}
+
+int number = GetNumber("Введите число: ");
+int rezult = MultipleNumers(number);
+Console.WriteLine(rezult);
+*/
+
+long GetNumber(string message)
+{
+    long result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (long.TryParse(Console.ReadLine(), out result) && result >=1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Число ввели некорректно.");
+        }
+    }
+    return result;
+}
+
+long MultipleNumbers (long number)
+{
+    long multi = 1;
+    for (long i=1; i<=number; i++)
+    {
+    multi=multi * i;
+    }
+    return multi;
+}
+
+long number = GetNumber("Введите положительное число");
+long result1 = MultipleNumbers(number);
+Console.WriteLine(result1);
