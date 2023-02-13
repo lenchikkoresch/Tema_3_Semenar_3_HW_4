950577a91c46182d447ea7c966c01ded2cc63351
lenschik.kor <lenschik.kor@yandex.ru>
2023-02-13 21:16:27 +0300

    Задача 29_1 вводит пользователь

---
+++ HW_4_Tema_3/Z_29_1/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и 
+выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+
+/*Start:
+Console.WriteLine("Введите массив из 8 чисел, разделенных пробелами, запятыми или точками");
+string text = Console.ReadLine()!;
+char[] separators = new char[] { ' ', '.', ',' };
+string[] splitArray = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+int n = 8;
+int[] arr = new int[n];
+int resultNumber = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+  if (int.TryParse(splitArray[i], out resultNumber))
+  {
+    arr[i] = resultNumber;
+  }
+  else
+  {
+    Console.WriteLine("Ввели не число или не корректное число. Повторите ввод!");
+    Console.WriteLine("-----------");
+    goto Start;
+  }
+}
+Console.WriteLine($"Введенный пользователем массив - это {string.Join(", ", arr)}");*/
+
+int [] GreatArray()
+{
+Console.WriteLine("Введите массив из 8 чисел, разделенных пробелами, запятыми или точками");
+string text = Console.ReadLine()!;
+char[] spearator = new char[] { ',','.',' '};
+string[] splitArray = text.Split(spearator, StringSplitOptions.RemoveEmptyEntries);
+int caunt =  splitArray.Length;
+int n = caunt;
+int[] arr = new int[n];
+int resultNumber = 0;
+
+for (int i = 0; i < caunt; i++)
+{
+  if (int.TryParse(splitArray[i], out resultNumber))
+  {
+    arr[i] = resultNumber;
+  }
+  else
+  {
+    Console.WriteLine("Ввели не число или не корректное число. Повторите ввод!");
+      }
+      }
+      return arr;
+}
+
+      
+
+void PrintArray (int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+     Console.Write($"{arr[i]}, ");   
+    }
+}
+int [] a = GreatArray();
+PrintArray(a);
