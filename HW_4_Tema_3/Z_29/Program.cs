775f064ce87e7ee8c9e96d91571b34ac532ccca0
lenschik.kor <lenschik.kor@yandex.ru>
2023-02-13 21:15:28 +0300

    Задача 29 случайные числа итог

---
+++ HW_4_Tema_3/Z_29/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и 
+выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+
+int [] GreatArray()
+{
+int []Array = new int [8];
+Random number = new Random();
+for (int i=0;i<Array.Length;i++)
+{
+   Array[i] = number.Next();
+   }
+   return Array;
+}
+
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+     Console.Write($"{array[i]}, ");   
+    }
+}
+int [] a = GreatArray();
+PrintArray(a);
+
+
+
